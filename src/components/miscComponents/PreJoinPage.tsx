a4d335e30a0aa3e7306bff0fece2591132ac870d
Prince-Addo <157974639+MediaSFU@users.noreply.github.com>
2024-12-16 13:40:52 -0700

    Bug fix for CE no-UI join/create action.

--- src/components/miscComponents/PreJoinPage.tsx
+++ src/components/miscComponents/PreJoinPage.tsx
@@ -383,8 +383,8 @@ const PreJoinPage: React.FC<PreJoinPageOptions> = ({
 
       const createData: CreateLocalRoomParameters = {
         eventID: eventID,
-        duration: parseInt(duration, 10),
-        capacity: parseInt(capacity, 10),
+        duration: payload.duration,
+        capacity: payload.capacity,
         userName: payload.userName,
         scheduledDate: new Date(),
         secureCode: secureCode,
@@ -497,7 +497,7 @@ const PreJoinPage: React.FC<PreJoinPageOptions> = ({
 
     if (localLink.length > 0 && !localLink.includes('mediasfu.com')) {
       const joinData: JoinLocalEventRoomParameters = {
-        eventID: eventID,
+        eventID: payload.meetingID,
         userName: payload.userName,
         secureCode: '',
         videoPreference: null,
